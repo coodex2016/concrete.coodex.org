684189093bd7e7933c490311ea1292a058201ca1
Davidoff Shen <jujus.shen@126.com>
2017-03-09 10:58:01 +0800

    增加AcceptableService和SPIFacade

--- 00.coodex.utilities/src/main/java/cc/coodex/util/AcceptableServiceSPIFacade.java
+++ 00.coodex.utilities/src/main/java/cc/coodex/util/AcceptableServiceSPIFacade.java
@@ -5,7 +5,7 @@ package cc.coodex.util;
  */
 public abstract class AcceptableServiceSPIFacade<Param_Type, T extends AcceptableService<Param_Type>> extends SPIFacade<T> {
 
-    T getServiceInstance(Param_Type param) {
+    public T getServiceInstance(Param_Type param) {
         for (T instance : getAllInstances()) {
             if (instance.accept(param))
                 return instance;
