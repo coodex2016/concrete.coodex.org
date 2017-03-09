e42ed64d0c7148bbcf2dcd900a9f3345d3c981f2
Davidoff Shen <jujus.shen@126.com>
2017-03-09 11:17:20 +0800

    增加AcceptableService和SPIFacade

--- 00.coodex.utilities/src/main/java/cc/coodex/util/AcceptableServiceSPIFacade.java
+++ 00.coodex.utilities/src/main/java/cc/coodex/util/AcceptableServiceSPIFacade.java
@@ -1,9 +1,15 @@
 package cc.coodex.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by davidoff shen on 2017-03-09.
  */
 public abstract class AcceptableServiceSPIFacade<Param_Type, T extends AcceptableService<Param_Type>> extends SPIFacade<T> {
+
+    private final static Logger log = LoggerFactory.getLogger(AcceptableServiceSPIFacade.class);
+
 
     public T getServiceInstance(Param_Type param) {
         for (T instance : getAllInstances()) {
@@ -13,6 +19,9 @@ public abstract class AcceptableServiceSPIFacade<Param_Type, T extends Acceptabl
         T instance = getDefaultProvider();
         if (instance.accept(param))
             return instance;
-        throw new RuntimeException("no service instance accept this: " + param);
+
+        log.warn("no service instance accept this: {}", param);
+
+        return null;
     }
 }
